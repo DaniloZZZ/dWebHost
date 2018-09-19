04ad639cdd68cefb933aac6b24fc516e7bc9e6a1
Danilozzz <lkv97dn@gmail.com>
2018-09-19 19:27:00 +0300

    node working, add ips via "node_addr':[..] post

--- Node.py
+++ Node.py
@@ -1,3 +1,4 @@
+
 import requests,time,json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import urllib
@@ -6,8 +7,6 @@ parser = argparse.ArgumentParser(
     description='run node')
 parser.add_argument('port',
                     help='port to run')
-parser.add_argument('add_key',
-                    help='new cuskey')
 args = parser.parse_args()
 
 def ask_for_data(n,key,client):
@@ -15,7 +14,9 @@ def ask_for_data(n,key,client):
         'key':key,
         'client':client
     })
-    r = requests.get('http://'+n+'?'+params)
+    path = 'http://'+n+'?'+params
+    print('asking for data from',path)
+    r = requests.get(path)
 
     if r.status_code==200:
         print("asking for data from %s success"%n)
@@ -23,15 +24,16 @@ def ask_for_data(n,key,client):
     else:
         print(r.status_code)
         print(r,n)
+        #return "ERROR"+str(r.status_code)
 SELF = ''
 
 class Server(BaseHTTPRequestHandler):
     # GET
-    storage = {'1':"test_data",args.add_key:'CUSTOM'+args.add_key}
-    neighbours = ['127.0.0.1:3880']
+    storage = {'1':"test_data"}
+    neighbours = []
     def do_GET(s):
         print("<<==**\n Got connection:",s.path)
-        params = s.get_params()
+        params = s.get_params(s.path)
         print('params of conn',s.client_address,params)
         key = params['key'][0]
         if key =='nodes_':
@@ -41,13 +43,15 @@ class Server(BaseHTTPRequestHandler):
         client = params.get('client')
         #direct = False
         if not client:
-            client = s.client_address[0]
+            client = s.client_address[0]+':'+str(args.port)
         else:
             #client = client[0]
-            nadr =client
+            nadr = s.client_address[0]+':'+str(args.port)
+            client = s.client_address[0]+':'+str(args.port)
+            # here we assume that port is the same
             if nadr not in s.neighbours:
                 print('appending neighbour',nadr)
-                s.neighbours+=nadr
+                s.neighbours+=[nadr]
             #direct = True
         data = s.storage.get(key) 
         print("data",data)
@@ -57,25 +61,50 @@ class Server(BaseHTTPRequestHandler):
                 if n!=SELF and n!=client:
                     # do not ask the client itself
                     data = ask_for_data(n,key,SELF)
-                    print("got data from",n,data)
-                    s._send_resp(data)
-                    return
-            s._send_resp('NOTHING')
+                    if data:
+                        print("got data from",n,data)
+                        s._send_resp(data)
+                        return
+            s._send_resp('NOTHING',400)
             return
         else:
             # todo: send directly to client
             print("sending my data back")
             s._send_resp(data)
             return
-
         for n in s.neighbours:
             neigh = s.get_neigh(n)
         if neigh:
             s.neighbours.append(neigh)
+            return
+
+    def do_POST(s):
+        print("<<==**\n Got post connection:",s.path)
+        content_length = int(s.headers['Content-Length']) # <--- Gets the size of data
+        post_data = s.rfile.read(content_length) # <--- Gets the data itself
+        datastr = str(post_data.decode('utf-8'))
+        print('***')
+        print("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n"%(
+                     str(s.path), str(s.headers),datastr))
+        print('***')
+        resp = ''
+        try:
+            parsed = json.loads(datastr)
+            print (parsed)
+            addr = parsed.get('node_addr')
+            if addr:
+                s.neighbours+=addr
+                resp = 'list_upd'
+            else:
+                s.storage.update(parsed)
+                resp = 'ok'
+        except Exception as e:
+            resp = e.message
+        s._send_resp(resp)
         return
 
-    def get_params(s):
-        parsed  =urllib.parse.urlparse(s.path)
+    def get_params(s,param_str):
+        parsed  =urllib.parse.urlparse(param_str)
         params =urllib.parse.parse_qs(parsed.query)
         return params
 
@@ -100,9 +129,9 @@ def run():
 
     print('starting server...')
     # Server settings
-    port =  int(args.port) or 3880
-    SELF = '127.0.0.1:'+str(port)
-    server_address = ('127.0.0.1',port)
+    port =  int(args.port) or 5000
+    SELF = '0.0.0.0:'+str(port)
+    server_address = ('0.0.0.0',port)
     httpd = HTTPServer(server_address,Server)
     print('running server on %i...'%port)
     httpd.serve_forever()
